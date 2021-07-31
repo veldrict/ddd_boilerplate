8151ba1fdc4081689fa453032e1c6137e2ade80b
Wibowo Raditya <wibowo@code.id>
2021-07-31 10:08:03 +0700

    boilerplate

---
+++ boilerplate/lib/domain/core/utils/list_utils.dart
@@ -0,0 +1,30 @@
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+
+class ListUtil<T> {
+  late IList<T> items;
+  ListUtil() {
+    items = <T>[].lock;
+  }
+  List<T> addOrReplaceItem(T item, {bool isReplace = false}) {
+    List<T> addNewItems = items.unlock;
+    if (items.length == 0) {
+      addNewItems.add(item);
+    } else {
+      if (isReplace && items.length > 1) {
+        addNewItems.removeLast();
+      }
+      addNewItems.add(item);
+    }
+    return addNewItems;
+  }
+
+  IList<T> addOrRenewList(List<T> datas, {int currentPage = 1}) {
+    List<T> addNewItems = items.unlock;
+    print('addOrRenewList ${items.length}');
+    if (currentPage == 1) {
+      addNewItems.clear();
+    }
+    addNewItems.addAll(datas);
+    return addNewItems.lock;
+  }
+}
